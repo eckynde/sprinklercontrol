175ea28b6babfac608c47c6e5b76d78b6866260c
Sebastian <rinderhagen.sebastian@gmail.com>
2020-09-04 15:37:42 +0200

    Added first line of code
    ^^

--- openweathermap/getWeatherData.py
+++ openweathermap/getWeatherData.py
@@ -1,1 +1,14 @@
-Test
+import requests
+
+city = input('Stadnamen hier eingeben:')
+#print('Dies ist ein Test! Ihre eingegebene Stadt war:{}!!!'.format(city))
+
+######Dieser API Key holt aktuelle Wetterdaten: api.openweathermap.org/data/2.5/weather?q={{STADT}}&units=metric&appid={{API_KEY}}'
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+print(res)
+print(data)
