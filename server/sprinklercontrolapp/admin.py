09e1c00bc49d70ea8a6e15fdbaffb4854db659d9
Luke <BlankemeyerL@web.de>
2020-09-07 14:32:56 +0200

    Model Registration on Admin Page and improvement of Models.py

--- server/sprinklercontrolapp/admin.py
+++ server/sprinklercontrolapp/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from sprinklercontrolapp.models import Sprinkler, Restriction, Error, SprinklerError, WaterQuantity, GeneralSetting, Irrigation
 
 # Register your models here.
+class SprinklerAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Sprinkler, SprinklerAdmin)
+
+
+class SprinklerErrorAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(SprinklerError, SprinklerErrorAdmin)
+
+
+class RestrictionAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Restriction, RestrictionAdmin)
+
+
+class ErrorAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Error, ErrorAdmin)
+
+
+class WaterQuantityAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(WaterQuantity, WaterQuantityAdmin)
+
+
+class GeneralSettingAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(GeneralSetting, GeneralSettingAdmin)
+
+
+class IrrigationAdmin (admin.ModelAdmin):
+    pass
+admin.site.register(Irrigation, IrrigationAdmin)
+
--- server/sprinklercontrolapp/models.py
+++ server/sprinklercontrolapp/models.py
@@ -1,49 +1,49 @@
 from django.db import models
 
 # Create your models here.
-class sprinkler(models.Model):
+class Sprinkler(models.Model):
     label = models.CharField(max_length=10)
     location = models.CharField(max_length=25)
     power = models.BooleanField()
 
 
-class restrictions(models.Model):
+class Restriction(models.Model):
     label = models.CharField(max_length=10)
     timeStart = models.DateTimeField()
     timeEnd = models.DateTimeField()
     description = models.CharField(max_length=25)
-    sprinklerId = models.ForeignKey(sprinkler,on_delete=models.CASCADE)
+    sprinklerId = models.ForeignKey(Sprinkler,on_delete=models.CASCADE)
 
 
-class waterQuantity(models.Model):
+class WaterQuantity(models.Model):
     label = models.CharField(max_length=10)
-    sprinklerId = models.ForeignKey(sprinkler,on_delete=models.CASCADE)
+    sprinklerId = models.ForeignKey(Sprinkler,on_delete=models.CASCADE)
     waterQuantity = models.DecimalField(max_digits=10, decimal_places=2)
     timeStart = models.DateTimeField()
     timeEnd = models.DateTimeField()
 
 
-class generalSettings(models.Model):
+class GeneralSettings(models.Model):
     label = models.CharField(max_length=10)
     description = models.CharField(max_length=25)
     value = models.CharField(max_length=10)
 
 
-class error(models.Model):
+class Error(models.Model):
     label = models.CharField(max_length=10)
     description = models.CharField(max_length=25)
 
 
-class sprinklerError(models.Model):
+class SprinklerError(models.Model):
     label = models.CharField(max_length=10)
-    sprinklerId = models.ForeignKey(sprinkler,on_delete=models.CASCADE)
-    errorId = models.ForeignKey(error,on_delete=models.CASCADE)
+    sprinklerId = models.ForeignKey(Sprinkler,on_delete=models.CASCADE)
+    errorId = models.ForeignKey(Error,on_delete=models.CASCADE)
     timeStart = models.DateTimeField()
     timeEnd = models.DateTimeField()
 
 
-class irrigation(models.Model):
+class Irrigation(models.Model):
     label = models.CharField(max_length=10)
     timeStart = models.DateTimeField()
     timeEnd = models.DateTimeField()
-    sprinklerId = models.ForeignKey(sprinkler,on_delete=models.CASCADE)
+    sprinklerId = models.ForeignKey(Sprinkler,on_delete=models.CASCADE)
