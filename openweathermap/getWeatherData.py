2f830b1915632210f2e18f9a81a9bc7cce17e014
Sebastian <rinderhagen.sebastian@gmail.com>
2020-09-10 10:05:04 +0200

    Added evaluation of weather data script
    - Currently not executable

--- openweathermap/getWeatherData.py
+++ openweathermap/getWeatherData.py
@@ -1,14 +1,50 @@
 import requests
+import json
 
-city = input('Stadnamen hier eingeben:')
+city = input('Stadtnamen hier eingeben:')
 #print('Dies ist ein Test! Ihre eingegebene Stadt war:{}!!!'.format(city))
 
 ######Dieser API Key holt aktuelle Wetterdaten: api.openweathermap.org/data/2.5/weather?q={{STADT}}&units=metric&appid={{API_KEY}}'
-url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='.format(city)
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&1h&dt&appid='.format(city)
 
 res = requests.get(url)
-
 data = res.json()
 
-print(res)
-print(data)
+##print(res)
+##print(data)
+
+## Prüfe ob Data-load erfolgreich
+if str(res)!="<Response [200]>" :
+    print("Data load = error")
+else:
+    print("Data load = success")
+
+    ## Prüfe ob Regen-Objekt vorhanden:
+    if "rain" in data:
+        rain1h = data['rain']['1h']
+    else:
+        rain1h = 0
+
+    ## Wolken
+    if "clouds" in data:
+        clouds = data['clouds']['all']
+
+    ## Wetter
+    if "weather" in data:
+        weather_id = data[0]['weather']['id']
+        weather_type = data['weather']['main']
+        weather_desc = data['weather']['description']
+        
+
+    ## Temperatur
+    if "main" in data:
+        temperature = data['main']['temp']
+
+    ## Time Stamps
+    if "dt" in data:
+        timeStamp_DataLoad = data['dt']
+    if "sys" in data:
+        timeStamp_Sunrise = data['sys']['sunrise']
+        timeStamp_Sunset = data['sys']['sunset']
+
+    print(rain1h)
