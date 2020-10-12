37e541ef5d72a8a7beebd799728cbad5da2e96aa
Maxi <maxi@stutzriemer.de>
2020-10-12 20:48:56 +0200

    New UI+loginPage

--- server/server/settings.py
+++ server/server/settings.py
@@ -120,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
