b4d5373c6d7211d60d0a5b8863523950289b93bc
Sebastian <rinderhagen.sebastian@gmail.com>
2020-10-12 17:28:47 +0200

    Adapted py script
    *now with hourly forecast (Still no parse though)

--- openweathermap/getWeatherData.py
+++ openweathermap/getWeatherData.py
@@ -5,64 +5,57 @@ import json
 import sys
 from weatherClass.weatherClass import weatherClass
 
-#Prüfen ob Parameter übergeben wurde
+apiKey = "ca55cf484b9838023ef2239091a6b5e9"
 
-print("______________________________________________________"+"\n")
-
-if not(len(sys.argv) != 2):
-    city = sys.argv[1]
-    #City im Skript angeben
-    #city = input('Stadtnamen hier eingeben:')
-
-    ######Dieser API Key holt aktuelle Wetterdaten: api.openweathermap.org/data/2.5/weather?q={{STADT}}&units=metric&appid={{API_KEY}}'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&1h&dt&appid=XXX'.format(city)
-
+##Get Longitude and Latitude
+def getLonLat(city):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&1h&dt&appid={}'.format(city, apiKey)
     res = requests.get(url)
     data = res.json()
 
-    ##print(res)
-    ##print(data)
-
-    ## Prüfe ob Data-load erfolgreich
     if str(res)=="<Response [200]>":
-        print("[ Data load = success ]"+"\n")
-
-        ## Regen in mm
-        # 1mm = 1L / m²:
-        if "rain" in data:
-            rain1h = data['rain']['1h']
-        else:
-            rain1h = 0
-
-        ## Wolken
-        if "clouds" in data:
-            clouds = data['clouds']['all']
-
-        ## Wetter
-        if "weather" in data:
-            weather_id = data['weather'][0]['id']
-            weather_type = data['weather'][0]['main']
-            weather_desc = data['weather'][0]['description']
-            
-        ## Temperatur
-        if "main" in data:
-            temperature = data['main']['temp']
-
-        ## Time Stamps
-        if "dt" in data:
-            timeStamp_DataLoad = data['dt']
-        if "sys" in data:
-            timeStamp_Sunrise = data['sys']['sunrise']
-            timeStamp_Sunset = data['sys']['sunset']
-
-        currentData = weatherClass(rain1h, clouds, weather_id, weather_type, weather_desc, \
-       temperature, timeStamp_DataLoad, timeStamp_Sunrise, timeStamp_Sunset)
-
-        currentData.printObject()
+        if "coord" in data:
+            return data['coord']['lon'], data['coord']['lat']
     else:
-        print("[ Data load = error ]"+"\n")
         if "message" in data:
             print(str(res)+": "+data['message'])
+            sys.exit(99)
+
+##Get Weather Data win Longitude and Latitude as JSON File
+def getWeatherJson(lon, lat):
+    url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&1h&dt&exclude=minutely, daily, alerts&appid={}'.format(lat, lon, apiKey)
+    res = requests.get(url)
+    data = res.json()
+
+    if str(res)=="<Response [200]>":
+        return data
+    else:
+        if "message" in data:
+            print(str(res)+": "+data['message'])
+            sys.exit(99)
+
+##Parse the whole fucking json and start DB Queries
+def parseJson(json):
+    print(json)
+
+
+
+
+
+
+print("______________________________________________________"+"\n")
+
+#Prüfen ob Parameter übergeben wurde
+if not(len(sys.argv) != 2):
+    city = sys.argv[1]
+    
+    try:
+        lon, lat = getLonLat(city)
+    except:
+        sys.exit(99)
+    else:
+        parseJson(getWeatherJson(lon, lat))
+
 else:
     print('[ Invalid Number of Arguments. ]')
 
