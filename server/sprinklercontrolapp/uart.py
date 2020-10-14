af0644ceb5eb6792a3c044a903367a0e28cb7eb8
Jannik Book <jannik.book@outlook.com>
2020-10-14 14:06:47 +0200

    Allow serializing `Model` classes as BSON
    Deserialization later, maybe.

--- server/sprinklercontrolapp/uart.py
+++ server/sprinklercontrolapp/uart.py
@@ -1,3 +1,4 @@
+import bson
 import logging
 from serial import Serial
 from django.conf import settings
@@ -12,3 +13,11 @@ def open_uart() -> Serial:
     uart = Serial(settings.UART.device, baudrate = settings.UART.baudrate)
     logger.info('Opened UART interface ' + uart.name)
     return uart    
+
+def bson_from_model(model_instance):
+    d = get_dict_except_state(model_instance)
+    return bson.dumps(d)
+
+def get_dict_except_state(obj):
+    d = obj.__dict__
+    return { k: d[k] for k in set(list(d.keys())) - set(['_state']) }
