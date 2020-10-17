0208e5e148c1c9d784078ee21270c9f7184a3ce1
eckende <jan@d-h-e.de>
2020-10-18 00:29:03 +0200

    Add preferences singleton to admin page

--- server/sprinklercontrolapp/admin.py
+++ server/sprinklercontrolapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from sprinklercontrolapp.models import Sprinkler, WeeklyRepeatingTimer, Weekday, IrrigationPlan
+from sprinklercontrolapp.models import Sprinkler, WeeklyRepeatingTimer, Weekday, IrrigationPlan, Preferences
 
 # Register your models here.
 class SprinklerAdmin (admin.ModelAdmin):
@@ -20,3 +20,15 @@ admin.site.register(Weekday, WeekdayAdmin)
 class IrrigationPlanAdmin (admin.ModelAdmin):
     pass
 admin.site.register(IrrigationPlan, IrrigationPlanAdmin)
+
+class SingletonAdmin(admin.ModelAdmin):
+    actions = None
+    def has_add_permission(self, request):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Preferences)
+class PreferencesAdmin(SingletonAdmin):
+    pass
