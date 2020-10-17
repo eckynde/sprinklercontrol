efd8abea8be25a00a07f15d7690d928ef2d8c7a2
Sebastian <rinderhagen.sebastian@gmail.com>
2020-10-17 14:42:32 +0200

    removed WeatherClass

---
+++ server/sprinklercontrolapp/controlSmartSprinkler.py
@@ -0,0 +1,8 @@
+from sprinklercontrolapp.models import WeatherCurrent, WeatherForecast, Sprinkler
+
+## "exec(open('sprinklercontrolapp/controlSmartSprinkler.py').read())"
+
+smartSprinklers = Sprinkler.objects.filter(mode='S')
+
+for objs in smartSprinklers:
+    print(x.label)
