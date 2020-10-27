9f8f10a730f7825f00016e0cc66d45d032a9ccdd
Jannik Book <jannik.book@outlook.com>
2020-10-27 03:42:22 +0100

    Create init script that adds weekdays to DB

---
+++ server/sprinklercontrolapp/management/commands/initialize.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from sprinklercontrolapp.models import Weekday
+
+class Command(BaseCommand):
+    help = 'Initializes the application before the first start'
+
+    def handle(self, *args, **options):
+        if Weekday.objects.count() > 0:
+            print('There are already objects in the Weekday table! Continue with initialization regardless? (y/N)')
+            
+            answer = input()
+            if answer != 'y' and answer != 'Y':
+                print('Aborted!')
+                return
+
+        print ('Initializing...')
+        Weekday.objects.create(label='Montag')
+        Weekday.objects.create(label='Dienstag')
+        Weekday.objects.create(label='Mittwoch')
+        Weekday.objects.create(label='Donnerstag')
+        Weekday.objects.create(label='Freitag')
+        Weekday.objects.create(label='Samstag')
+        Weekday.objects.create(label='Sonntag')
