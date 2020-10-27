68ab77517a80f0676a28751f8e8229f4d8c83af9
Jannik Book <jannik.book@outlook.com>
2020-10-27 19:58:45 +0100

    small setup adjustment

--- dontrun_setupdjango.bat
+++ dontrun_setupdjango.bat
@@ -3,6 +3,5 @@ py manage.py makemigrations
 py manage.py migrate
 py manage.py initialize
 
-set DJANGO_SUPERUSER_PASSWORD=book
-py manage.py createsuperuser --noinput
-py manage.py runserver
+set DJANGO_SUPERUSER_PASSWORD=password
+py manage.py createsuperuser --noinput --username admin
